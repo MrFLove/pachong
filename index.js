b24dbae537c8bf838dff571222024ef6ca08c812
unknown <yhda66@126.com>
2017-08-22 09:59:41 +0800

    新增忽略

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var request = require('request');
 var wordData = require('./word.json');
 wordData = wordData.word;
 var wordIndex = 0;
-var WordLength = 3;
+// var WordLength = wordData.length;
+var WordLength = 50;
 var dataArr = '';
 
 // console.log(wordData)
@@ -38,10 +39,11 @@ function getData(){//请求参数)
             // console.log('BODY: ' + chunk);  
         });
 
-
         res.on('end',function(){
             data = data.split("<script type='text/javascript'>window.parent.H.app.Chinese.callback(")[1].split(");</script>")[0];
             data = data.replace('result:"1",','');
+            // data = data.replace('chinese',);
+            data = '"'+wordData[wordIndex]+'":'+data
             if(wordIndex == WordLength){
                 dataArr += data;
             }else{
@@ -91,7 +93,7 @@ function dataJsonInit(callBack){
 //格式化数据
 function format(){
     console.log('开始格式化JSON文件')
-    dataArr = '['+dataArr+']';
+    dataArr = '{'+dataArr+'}';
     fs.writeFileSync('data.json',dataArr);
     downLoadAudios();
 }
@@ -117,10 +119,10 @@ function downLoadAudios(){
         //开始创建拼音字典
         fs.writeFileSync('./pinyindic.json',pinyinList);
         // 通过字典下载音频
-        downLoadAudios();        
+        requestAudios();        
     }
-
-    function downLoadAudios(){
+    //请求资源地址，准备下载
+    function requestAudios(){
         fs.readFile('./pinyindic.json',{encoding:'utf-8'}, function (err,bytesRead) {
             if (err) throw err;
             bytesRead = bytesRead.split(',');
@@ -129,7 +131,27 @@ function downLoadAudios(){
             // }
 
             // let fileUrl = 'http://xue.hahaertong.com/index.php/index.php?app=chinese&act=audio&audio=á';
-            var reqs = request.get()
+            // let filename = bytesRead[0]+'.mp3';
+            // let filepath = './audio'
+            // request
+            //   .get({
+            //     host:'',//请求发送到的服务器的域名或IP地址。默认为'localhost'。
+            //     hostname:'',//用于支持url.parse()。hostname比host更好一些
+            //     port:'',//远程服务器的端口。默认值为80。
+            //     localAddress:'',//用于绑定网络连接的本地接口。
+            //     socketPath:'',//Unix域套接字（使用host:port或socketPath）
+            //     method:'',//指定HTTP请求方法的字符串。默认为'GET'。
+            //     path:'',//请求路径。默认为'/'。如果有查询字符串，则需要包含。例如'/index.html?page=12'。请求路径包含非法字符时抛出异常。目前，只否决空格，不过在未来可能改变。
+            //     headers:'',//包含请求头的对象。
+            //     auth:'',//用于计算认证头的基本认证，即'user:password'
+            //     agent:'',//控制[Agent][]的行为。当使用了一个Agent的时候，请求将默认为Connection: keep-alive。可能的值为:
+            //     undefined:'',//在这个主机和端口上使用[全局Agent][]。
+            //     Agent:'',//在Agent中显式使用passed。
+            //     false:'',//在对Agent进行资源池的时候，选择停用连接，默认请求为:Connection: close。
+            //     keepAlive:{Boolean} '',//保持资源池周围的套接字在未来被用于其它请求。默认值为false
+            //     keepAliveMsecs:{Integer} ''//当使用HTTP KeepAlive的时候，通过正在保持活动的套接字发送TCP KeepAlive包的频繁程度。默认值为1000。仅当keepAlive被设置为true时才相关
+            //   })
+            //   .pipe(fs.createWriteStream('a1.mp3'))
             
         });
     }
@@ -145,9 +167,10 @@ function downLoadAudios(){
     }      
 }
 
+
 //初始化
 (function subjectInit(){
-    //清空data.json文件内容
+    //清空data.json文件内容 default
     dataJsonInit(function(){getData();});
     //临时调试
     // downLoadAudios();
